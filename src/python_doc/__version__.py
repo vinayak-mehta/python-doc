754197b8fa4c2e4e9d549176215e8ed949e2193b
Vinayak Mehta <vmehta94@gmail.com>
2020-09-30 06:47:07 +0530

    Bump version

--- src/python_doc/__version__.py
+++ src/python_doc/__version__.py
@@ -3,7 +3,7 @@
 __title__ = "python-doc"
 __description__ = "Open Python docs in the browser."
 __url__ = "https://github.com/vinayak-mehta/python-doc"
-__version__ = "2020.9.27"
+__version__ = "2020.9.30"
 __author__ = "Vinayak Mehta"
 __author_email__ = "vmehta94@gmail.com"
 __license__ = "Apache 2.0"
